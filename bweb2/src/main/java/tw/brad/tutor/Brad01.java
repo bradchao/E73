9aeb4f4313f54080cfb76e752de3f7831266d772
Brad <Brad@DESKTOP-HMKPLVE>
2023-12-06 10:07:17 +0800

    12061007

--- bweb2/src/main/java/tw/brad/tutor/Brad01.java
+++ bweb2/src/main/java/tw/brad/tutor/Brad01.java
@@ -1,20 +1,21 @@
 package tw.brad.tutor;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/HelloWorld")
-public class HelloWorld extends HttpServlet {
+@WebServlet("/Brad01")
+public class Brad01 extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-
-		response.getWriter().append("Served at: Brad ").append(request.getContextPath());
+		PrintWriter pw = response.getWriter();
+		pw.append("Brad").append("OK");
 	}
-
 
 }
