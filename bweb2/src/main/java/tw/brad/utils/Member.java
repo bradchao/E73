fef0295f7234059e1ba43766defc9613dd3778e6
Brad <Brad@DESKTOP-HMKPLVE>
2023-12-11 16:32:40 +0800

    12111632

---
+++ bweb2/src/main/java/tw/brad/utils/Member.java
@@ -0,0 +1,35 @@
+package tw.brad.utils;
+
+public class Member {
+	private int id;
+	private String account, passwd, cname;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	
+	@Override
+	public String toString() {
+		return account + ":" + cname;
+	}
+}
